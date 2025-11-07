272474b301b87e3eed068aebd0192828d971c238
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-07 23:05:46 +0700

    Menambahkan fungsi HomePage() untuk menampilkan halaman beranda aplikasi dengan tampilan penuh dan warna latar dari tema utama

--- app/src/main/java/com/example/questnavigasi/view/HomePage.kt
+++ app/src/main/java/com/example/questnavigasi/view/HomePage.kt
@@ -1,1 +1,8 @@
 package com.example.questnavigasi.view
+
+@Composable
+fun HomePage(navController: NavController) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) }

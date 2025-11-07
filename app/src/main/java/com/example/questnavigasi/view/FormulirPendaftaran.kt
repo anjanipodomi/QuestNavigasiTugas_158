41120f92cf321a2ad99fbf9e236972ce5e346bb0
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-07 23:28:55 +0700

    Menambahkan state variabel pada halaman FormulirPendaftaran untuk menyimpan input pengguna seperti nama, jenis kelamin, status, dan alamat, serta variabel showDialog untuk mengatur tampilan pop-up notifikasi setelah data disubmit

--- app/src/main/java/com/example/questnavigasi/view/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/questnavigasi/view/FormulirPendaftaran.kt
@@ -3,5 +3,8 @@ package com.example.questnavigasi.view
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormulirPendaftaran(navController: NavController) {
-    
-}
+    var nama by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var showDialog by remember { mutableStateOf(false) }

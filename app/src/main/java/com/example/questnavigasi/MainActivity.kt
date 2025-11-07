369bd79a40453cb048b7daeea8813de19a3c1a6e
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-07 22:49:00 +0700

    Menambahkan semua import yang dibutuhkan untuk menjalankan tampilan utama Compose, mengatur tema QuestNavigasi, dan menampilkan navigasi aplikasi (DataApp)

--- app/src/main/java/com/example/questnavigasi/MainActivity.kt
+++ app/src/main/java/com/example/questnavigasi/MainActivity.kt
@@ -1,4 +1,14 @@
 package com.example.questnavigasi
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.questnavigasi.ui.theme.QuestNavigasiTheme
+import com.example.questnavigasi.view.DataApp
 
 
 class MainActivity : ComponentActivity() {

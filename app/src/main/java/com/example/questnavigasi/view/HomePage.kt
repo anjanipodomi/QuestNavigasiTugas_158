2dc6d0586782907418b62d0d8f61b2fda429007d
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-07 23:15:15 +0700

    Menambahkan deklarasi package dan import untuk HomePage.kt, termasuk komponen Jetpack Compose seperti Text, Button, Image, serta warna tema Dongker agar halaman beranda dapat menampilkan elemen UI dengan gaya dan warna sesuai tema aplikasi

--- app/src/main/java/com/example/questnavigasi/view/HomePage.kt
+++ app/src/main/java/com/example/questnavigasi/view/HomePage.kt
@@ -1,4 +1,22 @@
 package com.example.questnavigasi.view
+package com.example.questnavigasi.view
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.questnavigasi.R
+import com.example.questnavigasi.ui.theme.Dongker
 
 @Composable
 fun HomePage(navController: NavController) {

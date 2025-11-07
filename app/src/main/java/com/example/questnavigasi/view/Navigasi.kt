fb24a11e22ad89396418726303d6fa758e3c143e
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-07 23:00:59 +0700

    Menghapus import HomePage yang tidak diperlukan di Navigasi.kt karena file sudah berada dalam package yang sama (com.example.questnavigasi.view).

--- app/src/main/java/com/example/questnavigasi/view/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasi/view/Navigasi.kt
@@ -1,22 +1,12 @@
 package com.example.questnavigasi.view
-package com.example.questnavigasi.view
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import com.example.questnavigasi.R
-import com.example.questnavigasi.ui.theme.Dongker
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 enum class Screen {
     homePage,
     listPeserta,

ec4704e7196eeaacd5e3a8a29f12117d74714e1a
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-07 23:18:48 +0700

    Menambahkan judul “List Daftar Peserta” dan dua kartu data peserta di halaman ListPeserta, lengkap dengan jarak antar elemen agar tampilan daftar terlihat rapi, terstruktur, dan mudah dibaca

--- app/src/main/java/com/example/questnavigasi/view/ListPeserta.kt
+++ app/src/main/java/com/example/questnavigasi/view/ListPeserta.kt
@@ -7,5 +7,15 @@ fun ListPeserta(navController: NavController) {
             .fillMaxSize()
             .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ) {
+        Text("List Daftar Peserta", color = Dongker, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(16.dp))
+        PesertaCard("Mohammad Zidan", "Laki-laki", "Lajang", "Manado")
+        Spacer(modifier = Modifier.height(10.dp))
+        PesertaCard("Meidina Yasmin", "Perempuan", "Kawin", "Yogyakarta")
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+    }
 }

05a868f87dfe54179a430d13a2a2a23fdd0fdc38
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-07 23:26:33 +0700

    Menambahkan import untuk ListPeserta.kt, termasuk komponen layout dan material seperti Column, Card, Button, serta warna tema Dongker agar halaman daftar peserta dapat menampilkan data dan tombol navigasi dengan gaya konsisten

--- app/src/main/java/com/example/questnavigasi/view/ListPeserta.kt
+++ app/src/main/java/com/example/questnavigasi/view/ListPeserta.kt
@@ -1,4 +1,13 @@
 package com.example.questnavigasi.view
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.questnavigasi.ui.theme.Dongker
 
 @Composable
 fun ListPeserta(navController: NavController) {

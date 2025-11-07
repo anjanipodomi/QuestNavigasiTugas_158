e14f3cbcc7dd14491a369d1f9e6f1f078147aaa9
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-07 23:50:51 +0700

    Menambahkan import untuk FormulirPendaftaran.kt, termasuk komponen layout, material, dan input seperti OutlinedTextField, Button, RadioButton, serta KeyboardOptions, agar halaman formulir dapat menerima input pengguna dan menampilkan elemen UI dengan rapi

--- app/src/main/java/com/example/questnavigasi/view/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/questnavigasi/view/FormulirPendaftaran.kt
@@ -1,4 +1,13 @@
 package com.example.questnavigasi.view
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable

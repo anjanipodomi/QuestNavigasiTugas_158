094687c048cbb262248b3c397a35a907cf755995
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-07 22:40:57 +0700

    membuat file HomePage, FormulirPendaftaran, ListPeserta, dan Navigasi, serta memperbaiki isi dimens xml dan strings xml sesuai susunan projek saya, serta menambahkan warna dongker di colors.kt

---
+++ app/src/main/java/com/example/questnavigasi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.questnavigasi
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/questnavigasi/view/FormulirPendaftaran.kt
@@ -0,0 +1,1 @@
+package com.example.questnavigasi.view
